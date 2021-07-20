fb5efe4f29a0f1307fbaa4eb04284041f8439a9d
Danish-Belal <danishexplore019@gmail.com>
2021-07-20 23:56:03 +0530

    Any Base Multiplication

--- Functions & Array/AnyBasedMul.cpp
+++ Functions & Array/AnyBasedMul.cpp
@@ -16,11 +16,80 @@ Sample Output
 43320
 
 */
+
 #include<bits/stdc++.h>
 using namespace std;
+
+int baseAddition(int b, int n1, int n2)
+{
+     int ans = 0;
+     int power =1;
+     int carry = 0;
+
+     while( n1 !=0 || n2!=0 || carry !=0){
+          int l1 = n1%10;
+          int l2 = n2%10;
+          n1 /=10;
+          n2 /=10;
+
+          int sum = l1+l2+carry;
+          int q = sum/b;
+          int r = sum%b;
+
+          carry = q;
+          ans += r *power;
+          power *= 10;
+
+     }
+     return ans;
+}
+
+
+int baseMul(int b, int n1, int d2)
+{
+     int ans = 0;
+     int power =1;
+     int carry = 0;
+
+     while( n1 !=0 ||  carry !=0){
+          int l1 = n1%10;
+        
+          n1 /=10;
+         
+
+          int mul = l1*d2+carry;
+          int q = mul/b;
+          int r = mul%b;
+
+          carry = q;
+          ans += r *power;
+          power *= 10;
+
+     }
+     return ans;
+}
+int getproduct(int b, int n1 , int n2)
+{
+  int ans =0;
+  int power = 1;
+  while (n2 !=0)
+  {
+    int d2 = n2%10;
+    n2 /=10;
+
+    int sprd = baseMul(b , n1 , d2);
+    ans = baseAddition(b , ans , sprd*power);
+    power *=10;
+  }
+  return ans;
+
+}
 int main()
 {
-     
+  int b, n1 ,n2;
+  cin>>b>>n1>>n2;
+  cout<<getproduct(b,n1,n2);
+
 
   return 0;
 }
