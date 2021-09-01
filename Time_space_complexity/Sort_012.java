16c002c50ec50636ebc73c038ea37a1f4669aefd
Danish-Belal <danishexplore019@gmail.com>
2021-09-01 20:42:07 +0530

    T C

--- Time_space_complexity/Sort_012.java
+++ Time_space_complexity/Sort_012.java
@@ -1,55 +1,66 @@
 package Time_space_complexity;
-import java.util.*;
+import  java.util.*;
 /*
-1. You are given an array(arr) containing only 0's and 1's.
-2. You have to sort the given array in increasing order and in linear time.
 Constraints
 
 1 <= N <= 10000
-arr[i] = 0,1
+arr[i] = 0,1,2
 
 Sample Input
 
-5
-0
+10
 1
 0
+2
+2
 1
 0
+2
+1
+0
+2
 
 Sample Output
 
-Swapping index 0 and index 0
+Swapping index 1 and index 0
+Swapping index 2 and index 9
+Swapping index 2 and index 8
 Swapping index 2 and index 1
-Swapping index 4 and index 2
+Swapping index 3 and index 7
+Swapping index 5 and index 2
+Swapping index 6 and index 6
 0
 0
 0
 1
 1
+1
+2
+2
+2
+2
  */
-public class Sort_01 {
-
-    public static void sort01(int[] arr){
+public class Sort_012 {
+    public static void sort012(int[] arr){
         //write your code here
+        int i = 0;
+        int j = 0;
+        int k = arr.length-1;
 
-         int i =0, j =0;
-
-       while( i < arr.length) {
-
-
-           if (arr[i] == 0) {
-               swap(arr, i, j);
-               i++;
-               j++;
-           }else{
-               i++;
-           }
-       }
-        }
-
-
-
+      while(i < k) {
+          if (arr[i] == 1) {
+              i++;
+          } else if (arr[i] == 0) {
+              swap(arr, i, j);
+              i++;
+              j++;
+          }
+          if (arr[i] == 2) {
+              swap(arr, i, k);
+              k--;
+          }
+      }
+    }
 
     // used for swapping ith and jth elements of array
     public static void swap(int[] arr, int i, int j) {
@@ -64,14 +75,14 @@ public class Sort_01 {
             System.out.println(arr[i]);
         }
     }
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
         for(int i = 0 ;i < n; i++){
             arr[i] = scn.nextInt();
         }
-        sort01(arr);
+        sort012(arr);
         print(arr);
     }
 }
