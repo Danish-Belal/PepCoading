e1ef75741a82214eb64295e3fa85b0d2a3e8132b
Danish-Belal <danishexplore019@gmail.com>
2021-09-29 23:14:06 +0530

    Bits Operations

--- BitwiseOperations/SetgetBit.java
+++ BitwiseOperations/SetgetBit.java
@@ -14,7 +14,17 @@ public class SetgetBit {
         // If number is not 0 then given bit is 1.
         if(andopr != 0) return 1;
         return 0;
+    }
 
+    public static int ClearBit(int n , int pos){
+        int mask = ~(1<<pos);
+        return n&mask;
+    }
+
+    static int UpdateBit(int n , int pos , int value){
+        int mask = ~(1<<pos);
+        n = n&mask;
+        return (n|(value<<pos));
 
     }
 
@@ -23,6 +33,9 @@ public class SetgetBit {
      //   System.out.println(bitpos);
 
         int setbit = SetBit(5 , 3);
-        System.out.println(setbit);
+       // System.out.println(setbit);
+
+        System.out.println(ClearBit(5,2));
+        System.out.println(UpdateBit(5,1,1));
     }
 }
