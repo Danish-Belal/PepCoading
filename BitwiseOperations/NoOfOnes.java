57462363e3257c528645a3742239c972def93eab
Danish-Belal <danishexplore019@gmail.com>
2021-09-30 12:24:37 +0530

    No of 1 in Binary Form

---
+++ BitwiseOperations/NoOfOnes.java
@@ -0,0 +1,16 @@
+package BitwiseOperations;
+
+public class NoOfOnes {
+    public static int Ones(int n){
+        int count = 0 ;
+        while(n !=0){
+            n = n & n-1;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Ones(16));
+    }
+}
