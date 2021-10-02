083521c5b8214193b2000fdac0645b1fd1e7f839
Danish-Belal <danishexplore019@gmail.com>
2021-10-03 01:45:19 +0530

    Sieve of Eratosthenes

--- Math/OptimizePrime.java
+++ Math/OptimizePrime.java
@@ -1,15 +1,17 @@
 package Math;
-
+/*
+https://leetcode.com/problems/count-primes/
+ */
 public class OptimizePrime {
     public static void main(String[] args) {
         int n = 40;
         boolean[] primes = new boolean[n+1];
-        isprime(n,primes);
+        sieve(n,primes);
 
     }
 
     // False in array means number is prime
-    public static void isprime(int n , boolean[] primes) {
+    public static void sieve(int n , boolean[] primes) {
         for(int i = 2 ; i*i <=n ; i++){
             if(!(primes[i])){
                 for (int j = i*2 ; j<=n ; j += i){
