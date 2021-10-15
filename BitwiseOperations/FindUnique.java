db9c2505caa80e0ef3a0affe1bf2bd9ddf2a7c56
Danish-Belal <danishexplore019@gmail.com>
2021-10-15 16:35:20 +0530

    Find unique element in Array

--- BitwiseOperations/FindUnique.java
+++ BitwiseOperations/FindUnique.java
@@ -8,6 +8,11 @@ public class FindUnique {
         int[] arr = {2,3,3,4,2,6,4 ,6,7};
         System.out.println(Unique(arr));
 
+
+        // Sorted Array,
+        int nums[] = {1,1,2,3,3,4,4};
+        System.out.println(uniquesorted(nums));
+
     }
     public static int Unique(int[] arr){
         int unique = 0;
@@ -15,5 +20,15 @@ public class FindUnique {
             unique ^=n;
         }
         return  unique;
+    }
+
+    // If array is sorted.
+    public static int uniquesorted(int[] nums){
+        int xor = nums[0];
+        for(int i=1 ; i< nums.length ; i++){
+            xor ^=nums[i];
+        }
+
+       return xor;
     }
 }
