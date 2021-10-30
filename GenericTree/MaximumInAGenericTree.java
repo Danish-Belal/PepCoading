e9661272f647b872cd870dd97f1c720d6f4fb9fb
Danish-Belal <danishexplore019@gmail.com>
2021-10-30 17:37:04 +0530

    Maximm in Generic Tree

---
+++ GenericTree/MaximumInAGenericTree.java
@@ -0,0 +1,94 @@
+package GenericTree;
+import java.io.*;
+import java.util.*;
+/*
+1. You are given a partially written GenericTree class.
+2. You are required to complete the body of max function. The function is expected to find the node with maximum value and return it.
+3. Input and Output is managed for you.
+
+Sample Input
+12
+10 20 -1 30 50 -1 60 -1 -1 40 -1 -1
+
+Sample Output
+60
+ */
+public class MaximumInAGenericTree {
+    private static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static void display(Node node) {
+        String str = node.data + " -> ";
+        for (Node child : node.children) {
+            str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+
+        for (Node child : node.children) {
+            display(child);
+        }
+    }
+
+    public static Node construct(int[] arr) {
+        Node root = null;
+
+        Stack<Node> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == -1) {
+                st.pop();
+            } else {
+                Node t = new Node();
+                t.data = arr[i];
+
+                if (st.size() > 0) {
+                    st.peek().children.add(t);
+                } else {
+                    root = t;
+                }
+
+                st.push(t);
+            }
+        }
+
+        return root;
+    }
+
+    public static int size(Node node) {
+        int s = 0;
+
+        for (Node child : node.children) {
+            s += size(child);
+        }
+        s += 1;
+
+        return s;
+    }
+
+    public static int max(Node node) {
+        // write your code here
+        int max = node.data;
+        for(Node child : node.children){
+            int msxinc = max(child);
+            max = Math.max(max , msxinc);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(values[i]);
+        }
+
+        Node root = construct(arr);
+        int m = max(root);
+        System.out.println(m);
+        // display(root);
+    }
+}
