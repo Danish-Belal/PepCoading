3a52acaf12d2ffff967f9cb64c59a6c9a627c7f4
Danish-Belal <danishexplore019@gmail.com>
2021-11-09 23:23:19 +0530

    Stati Example

---
+++ Oops/B/Massage.java
@@ -0,0 +1,11 @@
+package Oops.B;
+
+public class Massage {
+    public static void main(String[] args) {
+
+    }
+    public  static void massage(){
+        System.out.println("This is Package B");
+
+    }
+}
