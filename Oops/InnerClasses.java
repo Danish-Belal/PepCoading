1102d28ae450b6f8d08e80e07468671d36929c90
Danish-Belal <danishexplore019@gmail.com>
2021-11-10 22:58:44 +0530

    Inner Class and Outter class

---
+++ Oops/InnerClasses.java
@@ -0,0 +1,37 @@
+package Oops;
+
+// Outer classes can not be static .
+//static public InnseClasses{}
+
+//
+//
+//class Test{
+//    String name;
+//
+//    public Test(String name){
+//        this.name = name;
+//    }
+//}
+public class InnerClasses {
+
+//
+      static class Test{   // Test class object will not been called directly cause it is dependent on InnerClasses class , but it will work if we make it static,
+            String name;
+
+            public Test(String name){
+                this.name = name;
+            }
+        }
+
+        public static void main(String[] args) {
+            Test A = new Test("Danish");
+            Test B = new Test("Shahzeb");
+
+            System.out.println(A.name);
+            System.out.println(B.name);
+
+
+        }
+
+    }
+
