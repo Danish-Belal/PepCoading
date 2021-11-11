dc5ac5c2af35fa4102be28ba84c731b951e7f5d1
Danish-Belal <danishexplore019@gmail.com>
2021-11-11 12:43:19 +0530

    Main Class for Singleton class

---
+++ Oops/singleton/main.java
@@ -0,0 +1,10 @@
+package Oops.singleton;
+
+public class main {
+    public static void main(String[] args) {
+        Singleton obj1 = Singleton.getInstance();
+        Singleton ob2 = Singleton.getInstance();
+        Singleton obj3 = Singleton.getInstance();
+
+    }
+}
