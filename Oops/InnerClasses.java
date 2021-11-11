4eb24af8ed34aeaa101ffe33a614aac9258a740a
Danish-Belal <danishexplore019@gmail.com>
2021-11-11 12:41:31 +0530

    Many CLasses

--- Oops/InnerClasses.java
+++ Oops/InnerClasses.java
@@ -12,23 +12,33 @@ package Oops;
 //        this.name = name;
 //    }
 //}
+
 public class InnerClasses {
 
-//
       static class Test{   // Test class object will not been called directly cause it is dependent on InnerClasses class , but it will work if we make it static,
             String name;
 
             public Test(String name){
+
                 this.name = name;
             }
-        }
+
+            // This is known as Overriding .
+          @Override
+          public String toString() {
+              return name;
+          }
+
+      }
 
         public static void main(String[] args) {
             Test A = new Test("Danish");
             Test B = new Test("Shahzeb");
 
-            System.out.println(A.name);
-            System.out.println(B.name);
+//            System.out.println(A.name);
+//            System.out.println(B.name);
+
+            System.out.println(A);
 
 
         }
