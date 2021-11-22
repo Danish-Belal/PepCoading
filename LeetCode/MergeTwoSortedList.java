ac6e653adc8ca40292152efb871394522eef5884
Danish-Belal <danishexplore019@gmail.com>
2021-11-22 22:48:24 +0530

    Merge two Liniked Luist

---
+++ LeetCode/MergeTwoSortedList.java
@@ -0,0 +1,36 @@
+//package LeetCode;
+//
+///*
+// Merge two sorted linked lists and return it as a sorted list.
+// The list should be made by splicing together the nodes of the first two lists.
+// https://leetcode.com/problems/merge-two-sorted-lists/
+// */
+//
+//
+//
+//
+//
+//
+//public class MergeTwoSortedList {
+//
+//    public class ListNode {
+//     int val;
+//      ListNode next;
+//      ListNode() {}
+//      ListNode(int val) {
+//          this.val = val;
+//      }
+//      ListNode(int val, ListNode next)
+//      {
+//          this.val = val;
+//          this.next = next;
+//      }
+//
+//    }
+////    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+////        return ListNode n = new int[3];
+////    }
+//
+//    public static void main(String[] args) {
+//
+//}
