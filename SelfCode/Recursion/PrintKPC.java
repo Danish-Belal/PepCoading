064f491d85d45b8a107ec079a7a3316921d1e58a
Danish-Belal <danishexplore019@gmail.com>
2021-12-26 14:29:19 +0530

    Print KPC

--- SelfCode/Recursion/PrintKPC.java
+++ SelfCode/Recursion/PrintKPC.java
@@ -1,15 +1,23 @@
 package SelfCode.Recursion;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PrintKPC {
     static  String[] codes ={".;", "abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
 
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str= scn.nextLine();
+        KPC(str , "");
 
 
     }
     public static void KPC(String  str , String ans){
+        if(str.length() == 0){
+            System.out.print(ans +" ");
+            return;
+        }
 
         char ch = str.charAt(0);
         String ros = str.substring(1);
@@ -17,6 +25,8 @@ public class PrintKPC {
         String code = codes[ch - '0'];
         for (int i = 0 ; i <code.length() ; i++){
             char cc = code.charAt(i);
+            KPC(ros , ans+cc);
+          //  KPC(ros, ans);
            
         }
     }
