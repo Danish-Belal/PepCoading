38d39d58a9d8ee3dc8182e09d033f0a569514d5d
Danish-Belal <danishexplore019@gmail.com>
2021-12-29 16:10:28 +0530

    Target Sub Set

---
+++ SelfCode/Recursion/TargetSumSubset.java
@@ -0,0 +1,35 @@
+package SelfCode.Recursion;
+
+import java.util.Scanner;
+
+public class TargetSumSubset {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0 ;i <n ; i++){
+            arr[i] = scn.nextInt();
+        }
+        int target = scn.nextInt();
+        printTargetSumSubsets(arr , 0 , "", 0 , target );
+
+    }
+
+    // set is the subset
+    // sos is sum of subset
+    // tar is target
+    public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int tar) {
+        if(idx == arr.length){
+            if(sos == tar){
+                System.out.println(set + ".");
+            }
+            return;
+        }
+
+        printTargetSumSubsets(arr , idx+1 , set+arr[idx]+", " , sos+arr[idx] , tar);
+        printTargetSumSubsets(arr , idx+1 , set , sos , tar);
+
+
+    }
+}
