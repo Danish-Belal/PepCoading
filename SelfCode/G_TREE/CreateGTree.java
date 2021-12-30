5ef28ff6f28fa750bd2405f7d1985ba4cfa914a2
Danish-Belal <danishexplore019@gmail.com>
2021-12-30 22:29:46 +0530

    Generic Tree COnstruction

---
+++ SelfCode/G_TREE/CreateGTree.java
@@ -0,0 +1,52 @@
+package SelfCode.G_TREE;
+
+import GenericTree.Tree;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class CreateGTree {
+
+    public static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+    public static Node createTree(int[] arr){
+        Node root = new Node();
+        root.data = arr[0];
+
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+
+        for(int i = 1 ; i<arr.length ; i++){
+            if(arr[i] == -1){
+                st.pop();
+            }else{
+                Node node = new Node();
+                node.data = arr[i];
+                st.peek().children.add(node);
+                st.push(node);
+            }
+        }
+        return root;
+    }
+
+    public static void display(Node node){
+        String s = "";
+        s += node.data+" -> ";
+        for (Node child : node.children){
+            s += child.data + ", ";
+        }
+        s += ". ";
+        System.out.println(s);
+        for(Node child : node.children){
+            display(child);
+        }
+    }
+    public static void main(String args[]){
+        int[] arr = {10,20,50,-1 , 60 , -1,-1,30 , 70,-1,90,-1,-1,40,100,-1,-1,-1};
+        Node root = createTree(arr);
+        display(root);
+
+    }
+}
