44785c9bc415f201bf2d66193a37c420ef4a018d
Danish-Belal <danishexplore019@gmail.com>
2022-01-25 00:33:09 +0530

    Print Node B/W Range

---
+++ BinarySearchTree/PrintInRange.java
@@ -0,0 +1,156 @@
+package BinarySearchTree;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/*
+1. You are given a partially written BST class.
+2. You are required to complete the body of pir function.
+    "pir" function is expected to print all nodes between d1 and d2 (inclusive and in increasing order).
+3. Input and Output is managed for you.
+
+    Sample Input
+
+21
+50 25 12 n n 37 30 n n n 75 62 60 n n 70 n n 87 n n
+12
+65
+
+Sample Output
+
+12
+25
+30
+37
+50
+60
+62
+
+ */
+public class PrintInRange {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static Node construct(Integer[] arr) {
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+
+        int idx = 0;
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+
+                top.state++;
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+
+        return root;
+    }
+
+    public static void display(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        String str = "";
+        str += node.left == null ? "." : node.left.data + "";
+        str += " <- " + node.data + " -> ";
+        str += node.right == null ? "." : node.right.data + "";
+        System.out.println(str);
+
+        display(node.left);
+        display(node.right);
+    }
+    public static void print(Node node){
+        if(node == null) return;
+        System.out.println(node.data);
+        print(node.left);
+        print(node.right);
+    }
+
+    public static void pir(Node node, int d1, int d2) {
+        // write your code here
+        if(node == null)return;
+       pir(node.left , d1 , d2);
+       if(node.data >= d1 && node.data <= d2) System.out.println(node.data);
+       pir(node.right, d1 , d2);
+    }
+
+    static int sum = 0;
+    public static int rangeSumBST(Node root , int low , int high){
+        if(root == null) return 0;
+
+//        int l =rangeSumBST(root.left , low , high);
+//        if(root.data >= low && root.data <= high) sum +=root.data+l;
+//        int r = rangeSumBST(root.left , low , high);
+//        return sum+r;
+
+        return (rangeSumBST(root.left , low, high)+ rangeSumBST(root.right , low , high)+((root.data >= low && root.data <= high)? root.data : 0));
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            if (values[i].equals("n") == false) {
+                arr[i] = Integer.parseInt(values[i]);
+            } else {
+                arr[i] = null;
+            }
+        }
+
+        int d1 = Integer.parseInt(br.readLine());
+        int d2 = Integer.parseInt(br.readLine());
+
+        Node root = construct(arr);
+        pir(root, d1, d2);
+        int sum = rangeSumBST(root , d1,d2);
+        System.out.println(sum);
+    }
+}
