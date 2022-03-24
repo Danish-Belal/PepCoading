38a04783488b71a078802eb3fe4f92876c0fb11b
Danish-Belal <danishexplore019@gmail.com>
2022-03-24 12:52:16 +0530

    Boats TO Save People

---
+++ LeetCode/BoatsToSavePeople.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+    public static void main(String[] args){
+        int[] people = {1,2,3,4,5,6,7,8};
+        int limit = 8;
+        System.out.println(numRescueBoats(people , limit));
+
+    }
+    public static int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int count=0;
+
+        int l = 0;
+        int r = people.length-1;
+
+        while(l<=r){
+            int sum = people[l]+people[r];
+            if(sum <= limit){
+                l++;
+                r--;
+            }else{
+                r--;
+            }
+            count++;
+        }
+        return count;
+    }
+}
