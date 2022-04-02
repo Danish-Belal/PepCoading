9b6bf09e573024e39211e137588b6ac17803840b
Danish-Belal <danishexplore019@gmail.com>
2022-04-03 00:56:51 +0530

    Minimum Bit To Convert

--- LeetCode/MinBitFlipToConvert.java
+++ LeetCode/MinBitFlipToConvert.java
@@ -1,13 +1,14 @@
 package LeetCode;
 
 public class MinBitFlipToConvert {
-    public int minBitFlips(int start, int goal) {
+    public static void main(String[] args){
+        System.out.println(minBitFlips(10,7));
+    }
+    public static int minBitFlips(int start, int goal) {
 
         int target = start^goal;
-
-
         //   System.out.println(Integer.toBinaryString(target));
-        return Integer.bitCount(target);
+        return Integer.bitCount(target);    // Use to Count no of 1's in bits .
 
 
 
