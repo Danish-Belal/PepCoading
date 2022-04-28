c9e0d4113122a40bd389225269074d36bb2287d7
Danish-Belal <danishexplore019@gmail.com>
2022-04-28 17:00:42 +0530

    Min Cost Prims Algo

--- LeetCode/MinCost_Prims.java
+++ LeetCode/MinCost_Prims.java
@@ -9,7 +9,7 @@ https://leetcode.com/problems/min-cost-to-connect-all-points/
 You are given an array points representing integer coordinates of some points on a 2D-plane, where points[i] = [xi, yi].
 
 The cost of connecting two points [xi, yi] and [xj, yj]
-        is the manhattan distance between them: |xi - xj| + |yi - yj|, where |val| denotes the absolute value of val.
+    is the manhattan distance between them: |xi - xj| + |yi - yj|, where |val| denotes the absolute value of val.
 
 Return the minimum cost to make all points connected. All points are connected if there is exactly one simple path between any two points.
  */
