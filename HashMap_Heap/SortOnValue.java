430c46e28aff42578135d0ff89a4e05295f20d25
Danish-Belal <danishexplore019@gmail.com>
2022-05-05 23:28:20 +0530

    Sorting HashMap Based on Value

--- HashMap_Heap/SortOnValue.java
+++ HashMap_Heap/SortOnValue.java
@@ -1,6 +1,5 @@
 package HashMap_Heap;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -16,7 +15,9 @@ public class SortOnValue {
         System.out.println("Before Sorting");
         System.out.println(map);
 
-        LinkedHashMap<String , Integer> sortedMap = map.entrySet().stream().sorted(Comparator.comparingInt(Map.Entry::getValue)).collect(Collectors.toMap(Map.Entry::getKey , Map.Entry::getValue , (e1, e2)-> e1 , LinkedHashMap :: new));
+        LinkedHashMap<String , Integer> sortedMap = map.entrySet().stream().sorted((e1,e2)->{
+            return e1.getValue() - e2.getValue();
+        }).collect(Collectors.toMap(Map.Entry::getKey , Map.Entry::getValue , (e1,e2)-> e1 , LinkedHashMap :: new));
 
         System.out.println("After Sorting");
         System.out.println(sortedMap);
