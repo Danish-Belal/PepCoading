a1936d1306fed269222c9553e9eac8b1abefa18e
Danish-Belal <danishexplore019@gmail.com>
2022-05-17 21:58:07 +0530

    Crossponding Node

---
+++ LeetCode/FindCrosspondingNode.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+ */
+public class FindCrosspondingNode {
+
+     public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+    public static void main(String[] args){
+
+    }
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+
+         return null;
+    }
+}
