889f8299d9b013bcf8404f5c01f6bae926284149
Danish-Belal <danishexplore019@gmail.com>
2022-05-18 22:15:25 +0530

    Critical Connextion

---
+++ LeetCode/CriticalConnection.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+import java.util.List;
+
+/*
+https://leetcode.com/problems/critical-connections-in-a-network/
+There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections
+    forming a network where connections[i] = [ai, bi] represents a connection between servers ai and bi.
+    Any server can reach other servers directly or indirectly through the network.
+
+A critical connection is a connection that, if removed, will make some servers unable to reach some other server.
+
+Return all critical connections in the network in any order.
+ */
+public class CriticalConnection {
+    public static void main(String[] args){
+
+    }
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+
+        return null;
+    }
+}
