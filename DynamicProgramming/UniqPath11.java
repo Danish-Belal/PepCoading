0f9a16ef014cd8b3a61cc514bb8869905ad281ce
Danish-Belal <danishexplore019@gmail.com>
2022-05-21 01:02:19 +0530

    DYnamic Programming

--- DynamicProgramming/UniqPath11.java
+++ DynamicProgramming/UniqPath11.java
@@ -7,7 +7,8 @@ public class UniqPath11 {
                 {0,0,0}
         };
 
-        System.out.println(uniquePathsWithObstacles(grid2));
+       // System.out.println(uniquePathsWithObstacles(grid2));
+        System.out.println(uniquePathsWithObstaclesDP(grid2));
     }
     static int step;
     public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
@@ -28,5 +29,41 @@ public class UniqPath11 {
         }
         path(grid , sr+1 ,sc , dr,dc );
         path(grid , sr , sc+1 , dr ,dc );
+    }
+
+    public static  int uniquePathsWithObstaclesDP(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+
+        for(int i = 0 ; i<m ; i++){
+            if(grid[i][0] == 1) break;
+            else{
+                dp[i][0] = 1;
+            }
+        }
+
+        for(int i = 0 ; i<n ; i++){
+            if(grid[0][i] == 1) break;
+            else{
+                dp[0][i] = 1;
+            }
+        }
+
+        for(int i =1 ; i<m; i++){
+            for(int j = 1 ; j<n ; j++){
+                if(grid[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+
+
     }
 }
