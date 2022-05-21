1bc073cc4011fab8615e65b27fd574b50d742fbb
Danish-Belal <danishexplore019@gmail.com>
2022-05-22 00:27:40 +0530

    Moore's Voating algorithm

--- LeetCode/MajorityElement.java
+++ LeetCode/MajorityElement.java
@@ -1,5 +1,6 @@
 package LeetCode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 /*
@@ -42,10 +43,55 @@ public class MajorityElement {
         return maxfrech;
     }
 
+    // o(nlogn) Time and o(1) space complexity.
+    public static int majorityElement2(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for(int i = 0 ; i<n ;){
+            int val = nums[i];
+            int lidx = i;
+
+            while(lidx+1 <= n && nums[lidx] == val){
+                lidx++;
+            }
+            if( lidx-i > n/2) return nums[i];
+            i = lidx;
+        }
+
+        return 0;
+    }
+
+    // make a pair of unequal value . last remaning val will point to majority element.
+    static int mooreVoatingAlgo(int[] nums){
+        int n = nums.length;
+        int val = nums[0];
+        int count =1;
+
+        for(int i = 1 ; i<n ; i++){
+            if(nums[i] == val){
+                count++;
+            }else{
+                if(count > 0){
+                    // pair the unequal values so that reduce count value.
+                    count--;
+                }else{
+                    // if count is not available then make current value as a val.
+                    val = nums[i];
+                    count = 1;
+                }
+            }
+        }
+
+        return val;
+    }
+
     public static void main(String[] args){
        // int[] num = {1,1,2,2,2,3,3,3,5,6};
         int[] num = {10,10 , 30 ,30 , 30 , 30};
 
-        System.out.println(majorityElement(num));
+        //System.out.println(majorityElement(num));
+      //  System.out.println(majorityElement2(num));   //o(nlogn) time and(1) space complexity.
+        System.out.println(mooreVoatingAlgo(num));   //o(n) time and o(1) space complexity.
     }
 }
