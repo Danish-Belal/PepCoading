0a9ba4b1d7672c59249f3f51626922ecea916d2e
Danish-Belal <danishexplore019@gmail.com>
2022-05-28 21:54:18 +0530

    Next Greater Element

--- ArraysAndString2/NextGreaterElement3.java
+++ ArraysAndString2/NextGreaterElement3.java
@@ -5,71 +5,53 @@ import java.util.List;
 
 public class NextGreaterElement3 {
     public static void main(String[] args){
-
+        System.out.println(nextGreaterElement(12345));
     }
-    public int nextGreaterElement(int n) {
+    public static int nextGreaterElement(int n) {
         List<Integer> list = new ArrayList<>();
 
-        while(n > 0){
-            int val = n%10;
-            list.add(0,val);
-            n /=10;
+        while (n > 0) {
+            int val = n % 10;
+            list.add(0, val);
+            n /= 10;
 
         }
-
 
         int i = list.size()-2;
-
-        while(i >= 0){
-            if(list.get(i) > list.get(i+1)){
-                i--;
-            }else{
-                int val = check(list , i);
-                swap(list , i , val);
-                reverse(list , val+1);
-                return isInInt(list);
-            }
+        while(i>=0 && list.get(i) >= list.get(i+1)) i--;
+        if(i >=0){
+            int j = list.size()-1;
+            while(list.get(i) >= list.get(j)) j--;
+            swap(list , i , j);
+            reverse(list , i+1 , list.size()-1);
+            return  isInInt(list);
+           // return list;
         }
-
         return -1;
     }
 
-    int check(List<Integer> list , int i){
-        int j = list.size()-1;
-        int minIdx = 0;
-        while(j > i){
-            if(list.get(j) > list.get(i) ){
-                minIdx = j;
-            }
-            j--;
-        }
-        return minIdx;
+
+
+    private static void swap(List<Integer> nums, int i, int j) {
+        int t = nums.get(i);
+        nums.set(i,  nums.get(j));
+        nums.set(j , t);
     }
 
-    void swap(List<Integer> list , int i , int j){
-        int temp = list.get(i);
-        list.set(i, list.get(j));
-        list.set(j , temp) ;
+    private static void reverse(List<Integer> nums, int i, int j) {
+        while(i<j) swap(nums , i++ , j--);
     }
 
-    void reverse(List<Integer> list , int i){
-        int j = list.size()-1;
-        while(i<=j){
-            swap(list, i , j);
-            i++;
-            j--;
-        }
-    }
 
-    int isInInt(List<Integer> list){
+    static int isInInt(List<Integer> list){
 
         int ans  = 0;
         int i = 0 ;
         while(i < list.size()){
-            if(ans > 214748364 || ans < -214748364){
+            if(ans >= 214748364){
                 return -1;
             }
-            ans = ans*10+list.get(i);
+            ans = ans*10+list.get(i++);
         }
         return ans;
 
