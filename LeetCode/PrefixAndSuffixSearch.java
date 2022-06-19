145b6441e3b76a580b30cb88c452b16dbbce950b
Danish-Belal <danishexplore019@gmail.com>
2022-06-19 23:17:34 +0530

    Basic Codes

---
+++ LeetCode/PrefixAndSuffixSearch.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+public class PrefixAndSuffixSearch {
+
+    public static void main(String[] args){
+        String s = "Danish";
+        System.out.println(s.endsWith("Danish"));
+        System.out.println(Math.pow(2,31)-1);
+        System.out.println((int)1e9);
+
+
+    }
+
+}
