873e0783590e9538da9ec6046c5165948b9a4e18
Danish-Belal <danishexplore019@gmail.com>
2022-07-23 21:09:17 +0530

    Access Modifier

--- Java/AccessModfier.java
+++ Java/AccessModfier.java
@@ -1,10 +1,20 @@
 package Java;
 
 class Employee{
-
+    String name;
+    int age;
+    int id;
+    void getDetail(){
+        System.out.println("Name->"+name + " age->"+age + " Id->"+id );
+    }
 }
 public class AccessModfier {
     public static void main(String[] args){
+        Employee emp = new Employee();
+        emp.name = "Danish";
+        emp.id = 31;
+        emp.age = 21;
+        emp.getDetail();
 
     }
 }
