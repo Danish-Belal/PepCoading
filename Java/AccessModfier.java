67f7a50a76f400e2663d93b4945bef0b611e16ac
Danish-Belal <danishexplore019@gmail.com>
2022-08-01 22:51:12 +0530

    Access Modifier

--- Java/AccessModfier.java
+++ Java/AccessModfier.java
@@ -1,17 +1,20 @@
 package Java;
 
 class Employee{
-    String name;
+   private  String name;  // we can not access name directly so we will make a function to assignname.
     int age;
     int id;
     void getDetail(){
         System.out.println("Name->"+name + " age->"+age + " Id->"+id );
     }
+    void getName(String s){
+        name = s;
+    }
 }
 public class AccessModfier {
     public static void main(String[] args){
         Employee emp = new Employee();
-        emp.name = "Danish";
+        emp.getName  ("Danish");
         emp.id = 31;
         emp.age = 21;
         emp.getDetail();
